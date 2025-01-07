cde15dab08a08ec9d3f9eac2278f847393e637a4
Arina <arina.milyutina.0005@gmail.com>
2025-01-08 02:59:29 +0300

    added method findByBookID

--- src/main/java/com/example/libraryservice/repository/LibraryRepository.java
+++ src/main/java/com/example/libraryservice/repository/LibraryRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface LibraryRepository extends JpaRepository<Library,Long> {
+public interface LibraryRepository extends JpaRepository<Library, Long> {
     List<Library> findByReturnDateIsNull();
+
+    Optional<Library> findByBookID(Long bookId);
 }
 
