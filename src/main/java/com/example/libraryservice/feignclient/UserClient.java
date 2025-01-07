36a08693d624affca13635670abd554c114b524d
Arina <arina.milyutina.0005@gmail.com>
2025-01-08 02:57:20 +0300

    added method to interact with library2 (findByUsername)

--- src/main/java/com/example/libraryservice/feignclient/UserClient.java
+++ src/main/java/com/example/libraryservice/feignclient/UserClient.java
@@ -1,9 +1,11 @@
 package com.example.libraryservice.feignclient;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "library2")
 public interface UserClient {
-
-
+    @GetMapping("/user/find-by-username/{username}")
+    UserRequest findByUsername(@PathVariable String username);
 }
