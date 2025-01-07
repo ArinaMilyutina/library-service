172d628b5db34f7e6b32f9bba76be9a1fd068ccb
Arina <arina.milyutina.0005@gmail.com>
2025-01-07 18:32:52 +0300

    created SecurityConfiguration

---
+++ src/main/java/com/example/libraryservice/security/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.example.libraryservice.security;
+
+
+import com.example.libraryservice.jwt.JWTTokenFilter;
+import com.example.libraryservice.jwt.JWTTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private static final String ADMIN = "ADMIN";
+    private static final String BOOK_ENDPOINT = "/library/**";
+
+    @Bean
+    public JWTTokenFilter jwtTokenFilter() {
+        return new JWTTokenFilter(jwtTokenProvider);
+    }
+
+    @Autowired
+    private JWTTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .antMatchers(BOOK_ENDPOINT).hasAuthority(ADMIN)
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
+
