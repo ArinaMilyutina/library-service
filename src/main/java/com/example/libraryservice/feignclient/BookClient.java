256e251b46105b53465fddfc1b8a868f53923c80
Arina <arina.milyutina.0005@gmail.com>
2025-01-08 02:55:52 +0300

    added methods to interact with book-service (findByISBN and updateBookStatus)

--- src/main/java/com/example/libraryservice/feignclient/BookClient.java
+++ src/main/java/com/example/libraryservice/feignclient/BookClient.java
@@ -1,7 +1,16 @@
 package com.example.libraryservice.feignclient;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "book-service")
-public class BookClient {
+
+@FeignClient(name = "books-service")
+public interface BookClient {
+
+    @GetMapping("/book/find-by-isbn/{ISBN}")
+    BookRequest findByISBN(@PathVariable("ISBN") String ISBN);
+
+    @GetMapping("/book/status/{ISBN}")
+    void updateBookStatus(@PathVariable("ISBN") String ISBN);
 }
