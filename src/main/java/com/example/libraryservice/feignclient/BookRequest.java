7bd5e2b39e3c280a2233469529a510f6ec3a3f22
Arina <arina.milyutina.0005@gmail.com>
2025-01-08 21:26:24 +0300

    added status book to check the book dor availability

--- src/main/java/com/example/libraryservice/feignclient/BookRequest.java
+++ src/main/java/com/example/libraryservice/feignclient/BookRequest.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,4 +14,5 @@ import lombok.NoArgsConstructor;
 public class BookRequest {
     private Long bookId;
     private String title;
+    private Set<Status> status;
 }
---
+++ src/main/java/com/example/libraryservice/feignclient/Status.java
@@ -0,0 +1,7 @@
+package com.example.libraryservice.feignclient;
+
+public enum Status {
+    AVAILABLE,
+    UNAVAILABLE
+}
+
