258e52a9be115135db40bfcca4566baaa0d2fd0b
Arina <arina.milyutina.0005@gmail.com>
2025-01-08 21:28:38 +0300

    renamed field returnDate - > expectedReturnDate

--- src/main/java/com/example/libraryservice/dto/LibraryDto.java
+++ src/main/java/com/example/libraryservice/dto/LibraryDto.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -12,7 +14,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class LibraryDto {
     @Future(message = "Return date must be in the future")
-    private LocalDateTime returnDate;
+    @NotNull(message = "Expected return date cannot be blank")
+    private LocalDateTime expectedReturnDate;
     private String username;
     private String ISBN;
 }
