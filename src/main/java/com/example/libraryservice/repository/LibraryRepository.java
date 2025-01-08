7fa3eb9dc5fec7c1b22ad99317b6efa832584d87
Arina <arina.milyutina.0005@gmail.com>
2025-01-08 21:22:47 +0300

    deleted method findByReturnDateIsNull

--- src/main/java/com/example/libraryservice/repository/LibraryRepository.java
+++ src/main/java/com/example/libraryservice/repository/LibraryRepository.java
@@ -4,13 +4,10 @@ import com.example.libraryservice.entity.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LibraryRepository extends JpaRepository<Library, Long> {
-    List<Library> findByReturnDateIsNull();
-
     Optional<Library> findByBookID(Long bookId);
 }
 
