da679460b9f5be010d051ee96e1e3b75f727206c
Arina <arina.milyutina.0005@gmail.com>
2025-01-13 00:09:53 +0300

    changed the inherited class Exception -> RuntimeException

--- src/main/java/com/example/libraryservice/exception/NotFoundException.java
+++ src/main/java/com/example/libraryservice/exception/NotFoundException.java
@@ -1,7 +1,7 @@
 package com.example.libraryservice.exception;
 
 
-public class NotFoundException extends Exception {
+public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
     }
