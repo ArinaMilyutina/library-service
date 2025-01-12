4fc7417eea62ab295fa9eec14219498dd88d5b9d
Arina <arina.milyutina.0005@gmail.com>
2025-01-13 00:16:03 +0300

    changed method (added another criterion for search (bookId and actualReturnDate))

--- src/main/java/com/example/libraryservice/repository/LibraryRepository.java
+++ src/main/java/com/example/libraryservice/repository/LibraryRepository.java
@@ -4,10 +4,12 @@ import com.example.libraryservice.entity.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface LibraryRepository extends JpaRepository<Library, Long> {
-    Optional<Library> findByBookID(Long bookId);
+    Optional<Library> findByBookIDAndActualReturnDate(Long bookId, LocalDateTime actualReturnDate);
+
 }
 
